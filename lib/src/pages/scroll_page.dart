3a0a5d843b1021d5d7510b366df9cc2432a0b1ea
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-11-06 10:58:45 +0100

    Crear pagina con componentes PageView que permite deplazarse entre páginas

---
+++ lib/src/pages/scroll_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class ScrollPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        _pagina1(),
+        _pagina2(),
+      ],
+    ));
+  }
+
+  Widget _pagina1() {
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _pagina2() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(100, 200, 220, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(100, 200, 220, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/img/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(
+      color: Colors.white,
+      fontSize: 50.0,
+    );
+
+    return SafeArea(
+        child: Column(
+      children: [
+        Text('11 º', style: estiloTexto),
+        Text('Miércoles', style: estiloTexto),
+        Expanded(child: Container()),
+        Icon(
+          Icons.keyboard_arrow_down,
+          size: 70.0,
+          color: Colors.white,
+        )
+      ],
+    ));
+  }
+}
